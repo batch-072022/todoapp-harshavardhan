d41cd089ac9db6b56af89ca7b35426baac4c4b42
Harshavardhan Amula <harshavardhan.amula@valuemomentum.com>
2022-08-02 15:13:31 +0530

    Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { HomeComponent } from './home/home.component';
+import { AdTaskComponent } from './ad-task/ad-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    ListTasksComponent,
+    ListUsersComponent,
+    HomeComponent,
+    AdTaskComponent
   ],
   imports: [
     BrowserModule,
